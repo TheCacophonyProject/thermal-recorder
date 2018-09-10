ab90af3f5d497be5fb50666423070c04b09924fc
Clare McLennan <57052+clare@users.noreply.github.com>
2018-09-10 14:47:54 +1200

    Updated Motionprocessor tests too

--- cmd/thermal-recorder/motionprocessor_test.go
+++ cmd/thermal-recorder/motionprocessor_test.go
@@ -93,8 +93,8 @@ func TestRecorderNotTriggeredUnlessSeesMovement(t *testing.T) {
 
 func TestRecorderTriggeredAndHasPreviewAndMinNumberFrames(t *testing.T) {
 	recorder, scenarioMaker := SetupTest(DefaultTestConfig())
-	scenarioMaker.AddBackgroundFrames(10).AddMovingDotFrames(1).AddBackgroundFrames(40)
-	assert.Equal(t, FramesFrom(2, 36), recorder.GetRecordedFramesIds())
+	scenarioMaker.AddBackgroundFrames(11).AddMovingDotFrames(1).AddBackgroundFrames(40)
+	assert.Equal(t, FramesFrom(2, 37), recorder.GetRecordedFramesIds())
 }
 
 func TestRecorderNotTriggeredUntilTriggerFramesReached(t *testing.T) {
@@ -108,7 +108,7 @@ func TestRecorderNotTriggeredUntilTriggerFramesReached(t *testing.T) {
 
 	// not triggered by 3 moving frames in a row
 	scenarioMaker.AddMovingDotFrames(3).AddBackgroundFrames(40)
-	assert.Equal(t, FramesFrom(12, 48), recorder.GetRecordedFramesIds())
+	assert.Equal(t, FramesFrom(11, 48), recorder.GetRecordedFramesIds())
 }
 
 func TestRecorderNotStartedIfCheckCanRecordReturnsError(t *testing.T) {
@@ -123,11 +123,11 @@ func TestRecorderNotStartedIfCheckCanRecordReturnsError(t *testing.T) {
 func TestCanMakeMultipleRecordings(t *testing.T) {
 	recorder, scenarioMaker := SetupTest(DefaultTestConfig())
 
-	scenarioMaker.AddBackgroundFrames(10).AddMovingDotFrames(1).AddBackgroundFrames(39)
-	assert.Equal(t, FramesFrom(2, 36), recorder.GetRecordedFramesIds())
+	scenarioMaker.AddBackgroundFrames(11).AddMovingDotFrames(1).AddBackgroundFrames(39)
+	assert.Equal(t, FramesFrom(2, 37), recorder.GetRecordedFramesIds())
 
 	scenarioMaker.AddBackgroundFrames(10).AddMovingDotFrames(1).AddBackgroundFrames(39)
-	assert.Equal(t, FramesFrom(52, 86), recorder.GetRecordedFramesIds())
+	assert.Equal(t, FramesFrom(52, 87), recorder.GetRecordedFramesIds())
 }
 
 func TestMultipleRecordingsDontRepeatAnyFrames(t *testing.T) {
@@ -135,9 +135,9 @@ func TestMultipleRecordingsDontRepeatAnyFrames(t *testing.T) {
 	// recording then only the unwritten frames are recorded.
 	recorder, scenarioMaker := SetupTest(DefaultTestConfig())
 
-	scenarioMaker.AddBackgroundFrames(10).AddMovingDotFrames(1).AddBackgroundFrames(29)
-	assert.Equal(t, FramesFrom(2, 36), recorder.GetRecordedFramesIds())
+	scenarioMaker.AddBackgroundFrames(11).AddMovingDotFrames(1).AddBackgroundFrames(29)
+	assert.Equal(t, FramesFrom(2, 37), recorder.GetRecordedFramesIds())
 
 	scenarioMaker.AddMovingDotFrames(1).AddBackgroundFrames(39)
-	assert.Equal(t, FramesFrom(37, 66), recorder.GetRecordedFramesIds())
+	assert.Equal(t, FramesFrom(38, 67), recorder.GetRecordedFramesIds())
 }
