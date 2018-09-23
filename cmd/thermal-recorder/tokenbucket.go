ed6a70fa9c0b9b6e31c24d0a0f78037e67f92108
Clare McLennan <57052+clare@users.noreply.github.com>
2018-09-24 11:01:28 +1200

    Fixed help and added tests for token-bucket.

--- cmd/thermal-recorder/tokenbucket.go
+++ cmd/thermal-recorder/tokenbucket.go
@@ -7,6 +7,8 @@ type TokenBucket struct {
 	size   uint32
 }
 
+// AddTokens Adds the specified number of tokens to the bucket (or as many as it can without
+// overflowing the bucket)
 func (bucket *TokenBucket) AddTokens(newTokens uint32) {
 	bucket.tokens += newTokens
 	if bucket.tokens > bucket.size {
@@ -14,6 +16,8 @@ func (bucket *TokenBucket) AddTokens(newTokens uint32) {
 	}
 }
 
+// RemovesTokens Removes the specified number of tokens from the bucket, (or empties the bucket if it contains less
+// than the specified number)
 func (bucket *TokenBucket) RemoveTokens(oldTokens uint32) {
 	if bucket.tokens >= oldTokens {
 		bucket.tokens -= oldTokens
@@ -22,14 +26,17 @@ func (bucket *TokenBucket) RemoveTokens(oldTokens uint32) {
 	}
 }
 
+// HasTokens Returns true if the bucket has the specified number of tokens, else returns false.
 func (bucket *TokenBucket) HasTokens(tokens uint32) bool {
 	return bucket.tokens >= tokens
 }
 
+// Empty Empties the bucket
 func (bucket *TokenBucket) Empty() {
 	bucket.tokens = 0
 }
 
+// IsFull Returns true if the bucket is full, else returns false.
 func (bucket *TokenBucket) IsFull() bool {
 	return bucket.HasTokens(bucket.size)
 }
