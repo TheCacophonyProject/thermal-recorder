dc1f40ebae9b1726cb07a8740de16fc560b3792a
Clare McLennan <57052+clare@users.noreply.github.com>
2018-10-01 15:21:59 +1300

    Fixed after code review.

--- throttle/tokenbucket_test.go
+++ throttle/tokenbucket_test.go
@@ -10,7 +10,7 @@ type TestBucket struct {
 	TokenBucket
 }
 
-func (b *TestBucket) hasExactly(tokens float64) bool {
+func (b *TestBucket) hasApprox(tokens float64) bool {
 	return b.HasTokens(tokens) && !b.HasTokens(tokens+1)
 }
 
@@ -18,15 +18,15 @@ func TestTokenBucketCanAddTokens(t *testing.T) {
 	bucket := TestBucket{TokenBucket{size: 5}}
 
 	bucket.AddTokens(4)
-	assert.True(t, bucket.hasExactly(4))
+	assert.True(t, bucket.hasApprox(4))
 
 	bucket.AddTokens(0)
-	assert.True(t, bucket.hasExactly(4))
+	assert.True(t, bucket.hasApprox(4))
 	assert.False(t, bucket.IsFull())
 
 	// check cannot over-flow bucket
 	bucket.AddTokens(3)
-	assert.True(t, bucket.hasExactly(5))
+	assert.True(t, bucket.hasApprox(5))
 	assert.True(t, bucket.IsFull())
 }
 
@@ -36,11 +36,11 @@ func TestTokenBucketCanRemoveTokens(t *testing.T) {
 	bucket.AddTokens(4)
 
 	bucket.RemoveTokens(3)
-	assert.True(t, bucket.hasExactly(1))
+	assert.True(t, bucket.hasApprox(1))
 
 	// test removing too many tokens
 	bucket.RemoveTokens(3)
-	assert.True(t, bucket.hasExactly(0))
+	assert.True(t, bucket.hasApprox(0))
 }
 
 func TestTokenBucketEmptyRemovesAllTokensFromBucket(t *testing.T) {
@@ -48,5 +48,5 @@ func TestTokenBucketEmptyRemovesAllTokensFromBucket(t *testing.T) {
 
 	bucket.AddTokens(4)
 	bucket.Empty()
-	assert.True(t, bucket.hasExactly(0))
+	assert.True(t, bucket.hasApprox(0))
 }
