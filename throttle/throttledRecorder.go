2d354da49eea7fccbf84850f08a590ca5a2b37eb
Menno Finlay-Smits <inbox@menno.io>
2019-07-22 22:45:51 +1200

    Use a null throttle event listener if none is supplied
    Cleans up call sites.

--- throttle/throttledRecorder.go
+++ throttle/throttledRecorder.go
@@ -25,24 +25,6 @@ import (
 	"github.com/juju/ratelimit"
 )
 
-// ThrottledRecorder wraps a standard recorder so that it stops recording (ie gets throttled) if requested to
-// record too often.  This is desirable as the extra recordings are likely to be highly similar to the earlier recordings
-// and contain no new information.  It can happen when an animal is stuck in a trap or it is very windy.
-
-type ThrottledRecorder struct {
-	recorder           recorder.Recorder
-	listener           ThrottledEventListener
-	bucket             *ratelimit.Bucket
-	recording          bool
-	minRecordingLength int64
-	throttledFrames    uint32
-	frameCount         uint32
-}
-
-type ThrottledEventListener interface {
-	WhenThrottled()
-}
-
 func NewThrottledRecorder(
 	baseRecorder recorder.Recorder,
 	config *ThrottlerConfig,
@@ -62,17 +44,41 @@ func NewThrottledRecorderWithClock(
 	baseRecorder recorder.Recorder,
 	config *ThrottlerConfig,
 	minSeconds int,
-	eventListener ThrottledEventListener,
+	listener ThrottledEventListener,
 	clock ratelimit.Clock,
 ) *ThrottledRecorder {
 	bucket := ratelimit.NewBucketWithRateAndClock(config.RefillRate, int64(config.ThrottleAfter)*lepton3.FramesHz, clock)
+	if listener == nil {
+		listener = new(nullListener)
+	}
 	return &ThrottledRecorder{
 		recorder:           baseRecorder,
-		listener:           eventListener,
+		listener:           listener,
 		bucket:             bucket,
 		minRecordingLength: int64(minSeconds) * lepton3.FramesHz,
 	}
 }
+
+// ThrottledRecorder wraps a standard recorder so that it stops recording (ie gets throttled) if requested to
+// record too often.  This is desirable as the extra recordings are likely to be highly similar to the earlier recordings
+// and contain no new information.  It can happen when an animal is stuck in a trap or it is very windy.
+type ThrottledRecorder struct {
+	recorder           recorder.Recorder
+	listener           ThrottledEventListener
+	bucket             *ratelimit.Bucket
+	recording          bool
+	minRecordingLength int64
+	throttledFrames    uint32
+	totalFrames        uint32
+}
+
+type ThrottledEventListener interface {
+	WhenThrottled()
+}
+
+type nullListener struct{}
+
+func (lis *nullListener) WhenThrottled() {}
 
 func (throttler *ThrottledRecorder) CheckCanRecord() error {
 	return throttler.recorder.CheckCanRecord()
@@ -85,19 +91,17 @@ func (throttler *ThrottledRecorder) StartRecording() error {
 	} else {
 		throttler.recording = false
 		log.Print("Recording not started - currently throttled")
-		if throttler.listener != nil {
-			throttler.listener.WhenThrottled()
-		}
+		throttler.listener.WhenThrottled()
 		return nil
 	}
 }
 
 func (throttler *ThrottledRecorder) StopRecording() error {
 	if throttler.recording && throttler.throttledFrames > 0 {
-		log.Printf("Stop recording; %d/%d Frames throttled", throttler.throttledFrames, throttler.frameCount)
+		log.Printf("Stop recording; %d/%d frames throttled", throttler.throttledFrames, throttler.totalFrames)
 	}
 	throttler.throttledFrames = 0
-	throttler.frameCount = 0
+	throttler.totalFrames = 0
 
 	if throttler.recording {
 		throttler.recording = false
@@ -111,12 +115,12 @@ func (throttler *ThrottledRecorder) WriteFrame(frame *lepton3.Frame) error {
 		return nil
 	}
 
-	throttler.frameCount++
+	throttler.totalFrames++
 	if throttler.bucket.TakeAvailable(1) > 0 {
 		return throttler.recorder.WriteFrame(frame)
 	}
 
-	if throttler.throttledFrames == 0 && throttler.listener != nil {
+	if throttler.throttledFrames == 0 {
 		log.Printf("Recording throttled.")
 		throttler.listener.WhenThrottled()
 	}
