9a5a35e63cd3df4cbbd8d8d2cb956cfa5cd4233d
Giampaolo <g.ferraro22@gmail.com>
2020-09-15 14:56:56 +1200

    log config after motion threshold loaded for camera type

--- cmd/thermal-recorder/main.go
+++ cmd/thermal-recorder/main.go
@@ -85,11 +85,12 @@ func runMain() error {
 		return err
 	}
 
-	logConfig(conf)
-
 	conf.Verbose = args.Verbose
+
 	if args.TestCptvFile != "" {
 		results := NewCPTVPlaybackTester(conf).Detect(args.TestCptvFile)
+		logConfig(conf)
+
 		log.Printf("Detected: %-16s Recorded: %-16s Motion frames: %d/%d", results.motionDetectedFrames, results.recordedFrames, results.motionDetectedCount, results.frameCount)
 		return nil
 	}
@@ -146,6 +147,8 @@ func handleConn(conn net.Conn, conf *Config) error {
 
 	log.Printf("connection from %s %s (%dx%d@%dfps)", header.Brand(), header.Model(), header.ResX(), header.ResY(), header.FPS())
 	conf.LoadMotionConfig(header.Model())
+	logConfig(conf)
+
 	parseFrame := frameParser(header.Brand(), header.Model())
 	if parseFrame == nil {
 		return fmt.Errorf("unable to handle frames for %s %s", header.Brand(), header.Model())
