2345f11318852cad607604b793d8f8fb2150b977
Giampaolo <g.ferraro22@gmail.com>
2020-09-22 08:39:14 +1200

    added leptond check for first pixel being 0

--- cmd/leptond/main.go
+++ cmd/leptond/main.go
@@ -17,23 +17,25 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
-	"log"
-	"net"
-	"os/exec"
-	"strings"
-	"time"
-
-	"github.com/TheCacophonyProject/go-config"
-	"github.com/TheCacophonyProject/lepton3"
-	"github.com/TheCacophonyProject/thermal-recorder/headers"
 	arg "github.com/alexflint/go-arg"
 	"github.com/coreos/go-systemd/daemon"
 	"gopkg.in/yaml.v1"
+	"log"
+	"net"
+	"os/exec"
 	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/conn/gpio/gpioreg"
 	"periph.io/x/periph/host"
+	"strings"
+	"time"
+
+	"github.com/TheCacophonyProject/event-reporter/eventclient"
+	"github.com/TheCacophonyProject/go-config"
+	"github.com/TheCacophonyProject/lepton3"
+	"github.com/TheCacophonyProject/thermal-recorder/headers"
 )
 
 const (
@@ -43,6 +45,8 @@ const (
 	frameLogInterval         = 60 * 5 * framesHz
 
 	framesPerSdNotify = 5 * framesHz
+
+	telemetryBytes = 160 * 4 //this should be made public in lepton3
 )
 
 var version = "<not set>"
@@ -212,6 +216,15 @@ func runCamera(conf *Config, camera *lepton3.Lepton3, conn *net.UnixConn) error 
 			return &nextFrameErr{err}
 		}
 
+		if firstPixel(frame) == 0 {
+			event := eventclient.Event{
+				Timestamp: time.Now(),
+				Type:      "leptond",
+				Details:   map[string]interface{}{"description": map[string]interface{}{"details": "Bad Pixel (0,0)", "type": "leptond"}},
+			}
+			eventclient.AddEvent(event)
+			return errors.New("Bad pixel (0,0)")
+		}
 		if notifyCount++; notifyCount >= framesPerSdNotify {
 			daemon.SdNotify(false, "WATCHDOG=1")
 			notifyCount = 0
@@ -223,6 +236,9 @@ func runCamera(conf *Config, camera *lepton3.Lepton3, conn *net.UnixConn) error 
 	}
 }
 
+func firstPixel(frame []byte) uint16 {
+	return binary.BigEndian.Uint16(frame[telemetryBytes : telemetryBytes+2])
+}
 func logConfig(conf *Config) {
 	log.Printf("SPI speed: %d", conf.SPISpeed)
 	log.Printf("power pin: %s", conf.PowerPin)
