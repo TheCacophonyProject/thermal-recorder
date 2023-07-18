1d76ec911a71578b9c42140136d9f5d59c7bfb0c
cam <cameron.ryan.pears@gmail.com>
2023-07-19 09:57:17 +1200

    Removed leptond as it is replaced with tc2-agent

--- _release/postinstall.sh
+++ _release/postinstall.sh
@@ -3,8 +3,4 @@
 systemctl daemon-reload
 
 systemctl enable thermal-recorder.service
-systemctl enable leptond.service
-systemctl stop leptond.service
 systemctl restart thermal-recorder.service
-sleep 1
-systemctl start leptond.service
